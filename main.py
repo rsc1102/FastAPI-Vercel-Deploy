e81b6e6b3f34bd118d49cc0171329c1d7e935fc7
Rohit Chaudhari <46722995+rsc1102@users.noreply.github.com>
2024-09-29 04:01:48 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uuid
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket, client_id: str):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+        await self.broadcast(f"Client #{client_id} joined the chat.", websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str, sender: WebSocket):
+        for connection in self.active_connections:
+            if connection != sender:
+                await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def get(request: Request):
+    client_id = uuid.uuid4().hex[:10]
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"client_id":client_id}
+    )
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket, client_id)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}", websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat", websocket)
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    # Safely disconnect all WebSocket connections
+    for connection in manager.active_connections:
+        await connection.close()
